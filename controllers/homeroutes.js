ed2da43a28a16df1467c5980e28f7601d313be7c
AshLCorse <131395652+AshLCorse@users.noreply.github.com>
2024-08-12 12:07:55 -0600

    merge conf

--- controllers/homeroutes.js
+++ controllers/homeroutes.js
@@ -1,22 +1,28 @@
 const router = require("express").Router();
-const { User } = require("../models");
+const { User, Character } = require("../models");
 const withAuth = require("../utils/auth");
 
 router.get("/", withAuth, async (req, res) => {
-  try {
-    const userData = await user.findAll({
-      order: [["name", "ASC"]],
-    });
+  // If the user is not logged in, redirect the user to the login page
+  if (!req.session.loggedIn) {
+    res.redirect("/login");
+  } else {
+    // If the user is logged in, allow them to view the map
+    try {
+      const userData = await user.findAll({
+        order: [["name", "ASC"]],
+      });
 
-    const user = userData.map((project) => user.get({ plain: true }));
+      const user = userData.map((project) => user.get({ plain: true }));
 
-    res.render("character", {
-      user: req.user,
-      logged_in: req.session.logged_in,
-    });
-  } catch (err) {
-    console.error(err);
-    res.status(500).json(err);
+      res.render("homepage", {
+        user: req.user,
+        logged_in: req.session.logged_in,
+      });
+    } catch (err) {
+      console.error(err);
+      res.status(500).json(err);
+    }
   }
 });
 
--- seeds/seed.js
+++ seeds/seed.js
@@ -1,13 +1,13 @@
-const sequelize = require('../config/connection');
-const {user} = require('../models');
-const userData = require('./userData.json');
+const sequelize = require("../config/connection");
+const { User } = require("../models");
+const userData = require("./userData.json");
 
 const seedDatabase = async () => {
-    await sequelize.sync({force: true});
-    await user.bulkCreate(userData, {
-        individualHooks: true,
-        returning: true
-    });
-    process.exit(0);
-}
+  await sequelize.sync({ force: true });
+  await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+  });
+  process.exit(0);
+};
 seedDatabase();
