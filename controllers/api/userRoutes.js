2b9095adda8fc4c54e53104be7fb0c13899e3387
Jocephuss <rowlandjosiah14@gmail.com>
2024-08-12 21:21:06 -0600

    minor changes

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -50,7 +50,6 @@ router.post("/signup", async (req, res) => {
       id: userData.id,
       username: userData.username,
       password: userData.password_hash,
-      characters: [],
     });
     fs.writeFileSync(userDataPath, JSON.stringify(currentData, null, 2));
 
