c6f9df57e1ce3f10af533b76104a14d520b1dd88
AshLCorse <131395652+AshLCorse@users.noreply.github.com>
2024-08-12 21:29:58 -0600

    debug

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,7 +1,6 @@
 const router = require("express").Router();
 const fs = require("fs");
 const { User } = require("../../models");
-const fs = require("fs");
 const path = require("path");
 const userDataPath = path.join(__dirname, "../../seeds/userData.json");
 
