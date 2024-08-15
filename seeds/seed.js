5b8fa5b48a0098cd98a8daab3608da9fb22a45b8
AshLCorse <131395652+AshLCorse@users.noreply.github.com>
2024-08-14 18:17:27 -0600

    character attempt 1

--- seeds/seed.js
+++ seeds/seed.js
@@ -3,12 +3,17 @@ const sequelize = require(path.join(__dirname, "../config/connection"));
 const { User, Character } = require("../models");
 
 const userData = require("./userData.json");
+const characterData = require("./characterData.json");
 
 const seedDatabase = async () => {
   try {
     await sequelize.sync({ force: true });
 
     await User.bulkCreate(userData, {
+      individualHooks: true,
+      returning: true,
+    });
+    await Character.bulkCreate(characterData, {
       individualHooks: true,
       returning: true,
     });
